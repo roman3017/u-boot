b4b3369d6bf6180150ccbf89baab92231314cf20
Charles Papon <charles.papon.90@gmail.com>
2019-12-12 02:08:33 +0100

    Increase the heap to avoid malloc failure

--- include/configs/saxon.h
+++ include/configs/saxon.h
@@ -12,7 +12,7 @@
 /* assuming Text Base is not bottom of memory for INIT_SP to work */
 #define CONFIG_SYS_LOAD_ADDR CONFIG_SYS_TEXT_BASE
 #define CONFIG_SYS_INIT_SP_ADDR		CONFIG_SYS_LOAD_ADDR
-#define CONFIG_SYS_MALLOC_LEN		SZ_8K
+#define CONFIG_SYS_MALLOC_LEN		64*SZ_1K
 #define CONFIG_ENV_SIZE			SZ_4K
 #define	CONFIG_EXTRA_ENV_SETTINGS	"initrd_high=0xffffffff\0" \
                                   "fdt_high=0xffffffff\0"
