7498a1ba1cd308a7dc6f3daf35b9252a89d44cbe
roman3017 <rbacik@hotmail.com>
2020-02-02 10:53:50 -0800

    Saxon SPL related changes
     - replace hard coded clock with CONFIG_DEBUG_UART_CLOCK
     - add necessary definitions to saxon.h
     - remove redundant code from saxon.c
     - add spl_perform_fixups() to saxon.c
     - enable SPL in saxon_tang_defconfig

--- board/vexriscv/saxon/saxon.c
+++ board/vexriscv/saxon/saxon.c
@@ -5,14 +5,7 @@
  */
 
 #include <common.h>
-#include <cpu.h>
-#include <dm/ofnode.h>
-#include <dm.h>
-#include <mmc.h>
-#include <spi.h>
 #include <spl.h>
-
-DECLARE_GLOBAL_DATA_PTR;
 
 int board_init(void)
 {
@@ -21,6 +14,8 @@ int board_init(void)
 }
 
 #ifdef CONFIG_SPL_BUILD
+DECLARE_GLOBAL_DATA_PTR;
+
 void spl_board_init(void)
 {
 	debug("%s\n",__func__);
@@ -29,12 +24,6 @@ u32 spl_boot_device(void)
 {
 #ifdef CONFIG_SPL_MMC_SUPPORT
 	return BOOT_DEVICE_MMC1;
-#endif
-#ifdef CONFIG_SPL_XIP_SUPPORT
-	return BOOT_DEVICE_XIP;
-#endif
-#ifdef CONFIG_SPL_RAM_SUPPORT
-	return BOOT_DEVICE_RAM;
 #endif
 	return BOOT_DEVICE_NONE;
 }
@@ -45,21 +34,15 @@ int spl_start_uboot(void)
 	debug("%s\n",__func__);
 	return 0;
 }
-#endif
-
-#ifdef CONFIG_SPL_RAM_SUPPORT
-struct image_header *spl_get_load_buffer(ssize_t offset, size_t size)
+void spl_perform_fixups(struct spl_image_info *spl_image)
 {
-	return (struct image_header *)(CONFIG_SYS_LINUX_BASE + offset);
+	spl_image->os = IH_OS_LINUX;
+	spl_image->arg = (void *)CONFIG_SYS_SPL_ARGS_ADDR;
+	spl_image->entry_point = CONFIG_SYS_SPL_KERNEL_ADDR;
 }
-#endif
-
-#ifdef CONFIG_SPL_OS_BOOT
 void __noreturn jump_to_image_linux(struct spl_image_info *spl_image)
 {
-	spl_image->arg = (void *)CONFIG_SYS_FDT_BASE;
-	spl_image->entry_point = CONFIG_SYS_LINUX_BASE;
-	debug("Entering kernel: 0x%x, arg pointer: 0x%x\n", (u32)spl_image->entry_point, (u32)spl_image->arg);
+	printf("Entering kernel: 0x%x, arg pointer: 0x%x\n", (u32)spl_image->entry_point, (u32)spl_image->arg);
 	typedef void (*image_entry_arg_t)(ulong, void *)
 		__attribute__ ((noreturn));
 	image_entry_arg_t kernel =
@@ -95,51 +78,11 @@ int fdt_path_offset(const void *fdt, const char *path)
 	debug("%s\n",__func__);
 	return -1;
 }
-#endif /* !CONFIG_SPL_OF_LIBFDT */
-
-#endif /* CONFIG_SPL_BUILD */
-
-#if !CONFIG_IS_ENABLED(OF_CONTROL) || CONFIG_IS_ENABLED(OF_PLATDATA)
-int ofnode_read_u32(ofnode node, const char *propname, u32 *outp)
-{
-	debug("%s\n",__func__);
-	return 0;
-}
-u32 ofnode_read_u32_default(ofnode node, const char *propname, u32 def)
-{
-	debug("%s\n",__func__);
-	return def;
-}
-bool ofnode_read_bool(ofnode node, const char *propname)
-{
-	debug("%s\n",__func__);
-	return false;
-}
-ofnode ofnode_path(const char *path)
-{
-	debug("%s\n",__func__);
-	return ofnode_null();
-}
-ofnode ofnode_first_subnode(ofnode node)
-{
-	debug("%s\n",__func__);
-	return ofnode_null();
-}
-ofnode ofnode_next_subnode(ofnode node)
-{
-	debug("%s\n",__func__);
-	return ofnode_null();
-}
-const char *ofnode_read_string(ofnode node, const char *propname)
-{
-	debug("%s\n",__func__);
-	return 0;
-}
 int fdtdec_setup_mem_size_base(void)
 {
 	debug("%s\n",__func__);
-	gd->ram_base = 0x80000000;
-	gd->ram_size = 0x02000000;
+	gd->ram_base = CONFIG_SYS_SPL_RAM_BASE;
+	gd->ram_size = CONFIG_SYS_SPL_RAM_SIZE;
 	gd->ram_top = gd->ram_base + gd->ram_size - 1;
 	return 0;
 }
@@ -152,14 +95,10 @@ int fdtdec_setup_memory_banksize(void)
 	gd->bd->bi_dram[0].size = gd->ram_size;
 	return 0;
 }
-int fdtdec_get_alias_seq(const void *blob, const char *base, int offset, int *seqp)
-{
-	debug("%s\n",__func__);
-	return -1;
-}
 int dm_scan_fdt_dev(struct udevice *dev)
 {
 	debug("%s\n",__func__);
 	return 0;
 }
-#endif /*!CONFIG_IS_ENABLED(OF_CONTROL) || CONFIG_IS_ENABLED(OF_PLATDATA)*/
+#endif /* !CONFIG_SPL_OF_LIBFDT */
+#endif /* CONFIG_SPL_BUILD */
